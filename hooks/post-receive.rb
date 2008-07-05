4915efb84dd0482491cfad309c20a14d8c55835c
Antonin Amand <gwik@gwikzone.org>
2008-07-05 22:06:23 +0200

    update hook

--- hooks/post-receive.rb
+++ hooks/post-receive.rb
@@ -8,5 +8,7 @@ LIB_DIR = "/Users/gwik/dev/lighthouse-git-hooks"
 require LIB_DIR + "/lib/lighthouse/git_hooks"
 include Lighthouse::GitHooks
 
+rev_old, rev_new, ref = STDIN.read.split(" ")
+
 Configuration.load(CONFIG_DIR)
-TicketUpdate.new(ARGV[0], ARGV[1]).parse.send_changes
+TicketUpdate.new(rev_old, rev_new).parse.send_changes
